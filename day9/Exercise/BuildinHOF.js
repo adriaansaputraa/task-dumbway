172140c101d1d54fbea1b7d53225ce0d631d1b8e
Adrian Saputra <adriaansaputraa@gmail.com>
2023-07-16 19:32:49 +0700

    day 9 & day 10

---
+++ day9/Exercise/BuildinHOF.js
@@ -0,0 +1,25 @@
+    const data = [1,2,3,4,5,6,7,8,9,10]
+
+
+    //forEach
+    // data.forEach(function (value,index){
+    //     console.log("items: ",value)
+    //     console.log("index: ",index)
+    // })
+
+    //map
+    // const doubel = data.map((value,index)=>{
+    //     return value*2
+    // })
+
+    //filter
+    // const fillterData = data.filter((value,index) =>
+    //     value % 2 ==0)
+    // console.log(fillterData)
+
+    //reduce
+    // const reduce = data.reduce((prev,value) =>{
+    //     return prev + value
+    // })
+
+    // console.log(reduce)
