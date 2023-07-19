85fd250d70e0b3469f1b4181ce4f523fc462ad85
Adrian Saputra <adriaansaputraa@gmail.com>
2023-07-19 12:06:37 +0700

    day 9

---
+++ day9/Exercise/1.Asynchronous.js
@@ -0,0 +1,17 @@
+console.log("Hello")
+setTimeout(() => {
+    return console.log("Java Script 5 detik")
+},5000)//5 detik
+
+setTimeout(() => {
+    return console.log("Java Script 7 detik")
+},7000)//7 detik
+
+setTimeout(() => {
+    return console.log("Java Script 5 detik")
+},5000)//5 detik
+
+setTimeout(() => {
+    return console.log("Java Script 9 detik")
+},9000)//9 detik
+console.log("Coder")
