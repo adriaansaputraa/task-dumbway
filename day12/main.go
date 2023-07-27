03ca69fdb30116810d3cda41e588877345f75e88
Adrian Saputra <adriaansaputraa@gmail.com>
2023-07-27 22:14:28 +0700

    day 12 done

--- day12/main.go
+++ day12/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -16,6 +17,8 @@ type Project struct {
 	EndDate     string
 	Duration    string
 	Description string
+	PostDate    string
+	TimePost    string
 	Nodejs      bool
 	Golang      bool
 	ReactJs     bool
@@ -26,9 +29,12 @@ type Project struct {
 var dataProjects = []Project{
 	{
 		NameProject: "Project pertama",
-		StartDate:   "23/07/2023",
-		EndDate:     "25/08/2023",
+		StartDate:   "23-07-2023",
+		EndDate:     "25-08-2023",
+		Duration:    countDuration("23-07-2023", "25-08-2023"),
 		Description: "This is the description of project 1",
+		PostDate:    time.Now().Format("02-01-2006"),
+		TimePost:    time.Now().Format("15:04"),
 		Nodejs:      true,
 		Golang:      true,
 		ReactJs:     false,
@@ -36,10 +42,26 @@ var dataProjects = []Project{
 	},
 	{
 		NameProject: "Project kedua",
-		StartDate:   "23/07/2023",
-		EndDate:     "25/08/2023",
+		StartDate:   "23-07-2023",
+		EndDate:     "25-07-2023",
+		Duration:    countDuration("23-07-2023", "25-07-2023"),
 		Description: "This is the description of project 2",
+		PostDate:    time.Now().Format("02-01-2006"),
+		TimePost:    time.Now().Format("15:04"),
 		Nodejs:      false,
+		Golang:      true,
+		ReactJs:     true,
+		JavaScript:  true,
+	},
+	{
+		NameProject: "Project ketiga",
+		StartDate:   "23-07-2023",
+		EndDate:     "25-07-2024",
+		Duration:    countDuration("23-07-2023", "25-07-2024"),
+		Description: "This is the description of project 3",
+		PostDate:    time.Now().Format("02-01-2006"),
+		TimePost:    time.Now().Format("15:04"),
+		Nodejs:      true,
 		Golang:      true,
 		ReactJs:     true,
 		JavaScript:  true,
@@ -59,9 +81,11 @@ func main() {
 	e.GET("/form-project", formProject)
 	e.GET("/testimoni", testimonial)
 	e.GET("/project-detail/:id", projectDetail)
+	e.GET("/edit-project/:id", editProject)
 
 	//List Post
 	e.POST("/add-project", addProject)
+	e.POST("/edit-project/:id", postEditProject)
 	e.POST("/delete-project/:id", deleteProject)
 
 	//Server
@@ -129,7 +153,10 @@ func projectDetail(c echo.Context) error {
 				NameProject: data.NameProject,
 				StartDate:   data.StartDate,
 				EndDate:     data.EndDate,
+				Duration:    data.Duration,
 				Description: data.Description,
+				PostDate:    data.PostDate,
+				TimePost:    data.TimePost,
 				Nodejs:      data.Nodejs,
 				Golang:      data.Golang,
 				ReactJs:     data.ReactJs,
@@ -155,6 +182,48 @@ func formProject(c echo.Context) error {
 	return tmpl.Execute(c.Response(), nil) //execute = respons apa yang mau dipanggil
 }
 
+func postEditProject(c echo.Context) error {
+	id := c.Param("id")
+	idToInt, _ := strconv.Atoi(id)
+
+	addProjectName := c.FormValue("input-projectname")
+	addStartDate := c.FormValue("input-startdate")
+	addEndDate := c.FormValue("input-endDate")
+	addDescription := c.FormValue("input-descripton")
+	addNodeJs := c.FormValue("input-nodejs")
+	addGolang := c.FormValue("input-golang")
+	addReactJs := c.FormValue("input-reactjs")
+	addJavascript := c.FormValue("input-javascript")
+
+	startDate, err := time.Parse("2006-01-02", addStartDate)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	newStartDate := startDate.Format("02-01-2006")
+
+	endtDate, err := time.Parse("2006-01-02", addEndDate)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	newEndDate := endtDate.Format("02-01-2006")
+
+	var editProject = Project{
+		NameProject: addProjectName,
+		StartDate:   newStartDate,
+		EndDate:     newEndDate,
+		Duration:    countDuration(newStartDate, newEndDate),
+		Description: addDescription,
+		PostDate:    time.Now().Format("02-01-2006"),
+		TimePost:    time.Now().Format("15:04"),
+		Nodejs:      (addNodeJs == "on"),
+		Golang:      (addGolang == "on"),
+		ReactJs:     (addReactJs == "on"),
+		JavaScript:  (addJavascript == "on"),
+	}
+
+	dataProjects[idToInt] = editProject
+	return c.Redirect(http.StatusMovedPermanently, "/myProject")
+}
 func testimonial(c echo.Context) error {
 	tmpl, err := template.ParseFiles("view/Testimoni.html")
 
@@ -170,7 +239,7 @@ func deleteProject(c echo.Context) error {
 
 	dataProjects = append(dataProjects[:idToInt], dataProjects[idToInt+1:]...)
 
-	return c.Redirect(http.StatusMovedPermanently, "/myProject") //execute = respons apa yang mau dipanggil
+	return c.Redirect(http.StatusMovedPermanently, "/myProject")
 }
 
 func addProject(c echo.Context) error {
@@ -183,11 +252,26 @@ func addProject(c echo.Context) error {
 	addReactJs := c.FormValue("input-reactjs")
 	addJavascript := c.FormValue("input-javascript")
 
+	startDate, err := time.Parse("2006-01-02", addStartDate)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	newStartDate := startDate.Format("02-01-2006")
+
+	endtDate, err := time.Parse("2006-01-02", addEndDate)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	newEndDate := endtDate.Format("02-01-2006")
+
 	var newProject = Project{
 		NameProject: addProjectName,
-		StartDate:   addStartDate,
-		EndDate:     addEndDate,
+		StartDate:   newStartDate,
+		EndDate:     newEndDate,
+		Duration:    countDuration(newStartDate, newEndDate),
 		Description: addDescription,
+		PostDate:    time.Now().Format("02-01-2006"),
+		TimePost:    time.Now().Format("15:04"),
 		Nodejs:      (addNodeJs == "on"),
 		Golang:      (addGolang == "on"),
 		ReactJs:     (addReactJs == "on"),
@@ -196,11 +280,74 @@ func addProject(c echo.Context) error {
 
 	dataProjects = append(dataProjects, newProject)
 
+	fmt.Println("input StartDate", newStartDate)
+	fmt.Println("input EndDate", newEndDate)
 	fmt.Println("input Node Js", addNodeJs)
 	fmt.Println("input Golang", addGolang)
 	fmt.Println("input ReactJs", addReactJs)
 	fmt.Println("input JavaScript", addJavascript)
 
 	return c.Redirect(http.StatusMovedPermanently, "/myProject")
+
+}
+
+func countDuration(d1, d2 string) string {
+	date_1, _ := time.Parse("02-01-2006", d1)
+	date_2, _ := time.Parse("02-01-2006", d2)
+
+	diff := date_2.Sub(date_1)
+	days := int(diff.Hours() / 24)
+	weeks := days / 7
+	months := days / 30
+
+	if months >= 12 {
+		return strconv.Itoa(months/12) + " Tahun"
+	}
+	if months > 0 {
+		return strconv.Itoa(months) + " Bulan"
+	}
+	if weeks > 0 {
+		return strconv.Itoa(weeks) + " Minggu"
+	}
+
+	return strconv.Itoa(days) + " Hari"
+}
+
+func editProject(c echo.Context) error {
+	id := c.Param("id")
+	idToInt, _ := strconv.Atoi(id)
+
+	var ProjectDetail = Project{}
+
+	for index, data := range dataProjects {
+		if idToInt == index {
+			ProjectDetail = Project{
+				NameProject: data.NameProject,
+				StartDate:   data.StartDate,
+				EndDate:     data.EndDate,
+				Duration:    data.Duration,
+				Description: data.Description,
+				PostDate:    data.PostDate,
+				TimePost:    data.TimePost,
+				Nodejs:      data.Nodejs,
+				Golang:      data.Golang,
+				ReactJs:     data.ReactJs,
+				JavaScript:  data.JavaScript,
+			}
+		}
+	}
+	fmt.Println("Received data:", ProjectDetail.NameProject, ProjectDetail.StartDate)
+
+	data := map[string]interface{}{
+		"Id":      id,
+		"Project": ProjectDetail,
+	}
+	tmpl, err := template.ParseFiles("view/Edit-MyProject.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return tmpl.Execute(c.Response(), data)
 
 }
